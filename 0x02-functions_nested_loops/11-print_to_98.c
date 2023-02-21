43134fe6332e3734f5e81435bb3fe2a7f4edc1e9
Lettu100 <wiseletson@gmail.com>
2023-02-21 14:32:14 -0800

    final man

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -12,13 +12,13 @@ void print_to_98(int n)
 	{
 		for (i = n; i < 98; i++)
 		{
-			_putchar("%d, ", i);
+			_putchar("%d ", i);
 		}
 	} else
 	{
 		for (i = n; i > 98; i--)
 		{
-			_putchar("%d, ", i);
+			_putchar("%d ", i);
 		}
 	}
 	_putchar("98\n");
