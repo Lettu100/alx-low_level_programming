31933cc9b66e5d9f7d83d37139eded33c98d34ec
Lettu100 <wiseletson@gmail.com>
2023-02-21 11:32:13 -0800

    islower checker updated

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,8 +1,10 @@
 #include "main.h"
+
 /**
 * islower -> checks if the alphabet isa lowercase
 * Return : returns 0 or 1 depending on the condition
 */
+
 int _islower(int c)
 {
 	if (c >= 'a' && c <= 'z')
